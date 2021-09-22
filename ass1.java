b5bdea8a2a172e126a2b45cd242f8936b5fcd15a
atonalcat <55767105+atonalcat@users.noreply.github.com>
2021-09-21 22:56:42 -0700

    Add files via upload

--- ass1.java
+++ ass1.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class ass1 here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Chauncey Li)
+ * @version (9/21/2021)
  */
 public class ass1
 {
